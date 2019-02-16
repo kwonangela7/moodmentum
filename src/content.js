da5268ce99f7e57ad371f6a26067bd185cfc967e
vkhuang <vkhuang@berkeley.edu>
2019-02-16 11:10:18 -0800

    added skeleton for content.js and moved manifest.json

---
+++ src/content.js
@@ -0,0 +1,9 @@
+/* File: content.js
+ * ---------------
+ * Hello! You'll be making most of your changes
+ * in this file. 
+ *
+ * This file contains javascript code that is executed
+ * everytime a webpage loads over HTTP or HTTPS.
+ */
+
